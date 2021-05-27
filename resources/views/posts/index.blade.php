6fa33cd2f807b80131bc1ac8e7ee14af2bccf0c1
Vit Safarik <vit@safarik.dev>
2021-05-27 22:28:54 +0200

    add tailwindcss

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    Index
+    <div class="flex justify-center">
+        <div class="w-8/12 p-6 bg-gray-200 rounded-lg">
+            <h1>Content</h1>
+        </div>
+    </div>
 @endsection
